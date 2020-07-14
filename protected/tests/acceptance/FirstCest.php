88303691d72d56e364a7c879964f66862a76158b
Ken <kencho.gigascience@gmail.com>
2020-07-14 15:30:46 +0800

    Add acceptance test

---
+++ protected/tests/acceptance/FirstCest.php
@@ -0,0 +1,16 @@
+<?php 
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function frontpageWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Home');
+    }
+}
+
