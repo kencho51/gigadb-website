04cc27869c2b2a8db77c0a34863ab62fd54e9412
Ken <kencho.gigascience@gmail.com>
2021-07-20 13:47:06 +0800

    Update tools dir

---
+++ gigadb/app/tools/dataset-backup-tool/scripts/test_rclone.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ls remote files/dir
+#rclone --config=scripts/.rclone.conf ls test-cos-mac:
+
+# See all buckets
+rclone --config=scripts/.rclone.conf lsd test-cos-mac:
+
+# memory test remote
+#rclone --config=scripts/.rclone.conf test memory test-cos-mac:
+
+# Checks the files in the source test/_data dir and remote
+#rclone --config=scripts/.rclone.conf check tests/_data test-cos-mac:
+
+# Test sync dry run
+#rclone --config=scripts/.rclone.conf sync --progress --dry-run tests/_data test-cos-mac:
