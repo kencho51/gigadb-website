e935c96b703e86d2745f1f1102776bd3e23485a2
Ken <kencho.gigascience@gmail.com>
2021-07-20 14:20:23 +0800

    Add rclone cmd

--- gigadb/app/tools/dataset-backup-tool/scripts/test_rclone.sh
+++ gigadb/app/tools/dataset-backup-tool/scripts/test_rclone.sh
@@ -4,13 +4,16 @@
 #rclone --config=scripts/.rclone.conf ls test-cos-mac:
 
 # See all buckets
-rclone --config=scripts/.rclone.conf lsd test-cos-mac:
+#rclone --config=scripts/.rclone.conf lsd test-cos-mac:
 
 # memory test remote
-#rclone --config=scripts/.rclone.conf test memory test-cos-mac:
+rclone --config=scripts/.rclone.conf test memory test-cos-mac:
 
 # Checks the files in the source test/_data dir and remote
 #rclone --config=scripts/.rclone.conf check tests/_data test-cos-mac:
 
 # Test sync dry run
 #rclone --config=scripts/.rclone.conf sync --progress --dry-run tests/_data test-cos-mac:
+
+# Prints quota information about a remote to standard
+#rclone about --config=scripts/.rclone.conf test-cos-mac:
