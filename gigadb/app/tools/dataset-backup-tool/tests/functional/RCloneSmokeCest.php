400cbc31c1405511cd8e6508a631c041ad497147
Ken <kencho.gigascience@gmail.com>
2021-07-20 14:19:57 +0800

    To list bucket id

--- gigadb/app/tools/dataset-backup-tool/tests/functional/RCloneSmokeCest.php
+++ gigadb/app/tools/dataset-backup-tool/tests/functional/RCloneSmokeCest.php
@@ -1,7 +1,19 @@
 <?php
 
+use Yii;
+use yii\console\ExitCode;
 
 class RCloneSmokeCest
 {
 
+
+    /**
+     * Make sure rclone could be called
+     */
+    public function listBucketId(\FunctionalTester $I) {
+        $I->runShellCommand("rclone --config=scripts/.rclone.conf lsd test-cos-mac:");
+
+        $output = $I->grabShellOutput();
+        $I->assertStringContainsString('testrclonegznoversion-1306096270', $output, 'testrclonegznoversion-1306096270 is not found');
+    }
 }
