726397eccae2e260aeace101eb51bb047a4d5638
Ken <kencho.gigascience@gmail.com>
2021-07-28 12:15:31 +0800

    Add actual path

--- gigadb/app/tools/dataset-backup-tool/scripts/sync_files_prod.sh
+++ gigadb/app/tools/dataset-backup-tool/scripts/sync_files_prod.sh
@@ -4,5 +4,5 @@ set -e
 set -u
 
 echo "Backup to Tencet COS starts at" `date`
-rclone --version --checksum sync /data/gigadb/pub/10.5524/ cos:cngbdb-share-backup-2-1255501786/cngbdb/giga/gigadb/
+rclone --version --checksum sync /data/gigadb/pub/10.5524/ cos:cngbdb-share-backup-2-1255501786/cngbdb/giga/gigadb/pub/10.5524/
 echo "Backup to Tencent COS completed at" `date`
